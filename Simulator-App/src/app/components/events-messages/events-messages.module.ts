df87e86990de7bf4112f9184f095a90787cf108c
TRAORE <mohamed.traore@cgi.com>
2019-07-10 16:45:10 +0200

    remove in conflict

--- Simulator-App/src/app/components/events-messages/events-messages.module.ts
+++ Simulator-App/src/app/components/events-messages/events-messages.module.ts
@@ -2,25 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EventsMsgComponent } from './events-msg/events-msg.component';
 import { MaterialModule } from 'src/app/material.module';
-<<<<<<< HEAD
+import { FlightInfoComponent } from './flight-info/flight-info.component';
 import { EventTypeMsgComponent, EventTypeMsgDialogComponent } from './event-type-msg/event-type-msg.component';
 
 @NgModule({
-  declarations: [EventsMsgComponent, EventTypeMsgComponent, EventTypeMsgDialogComponent],
-=======
-import { FlightInfoComponent } from './flight-info/flight-info.component';
-
-@NgModule({
-  declarations: [EventsMsgComponent, FlightInfoComponent],
->>>>>>> 910dcd7dd37f9903b959e8f312d92d3bad90d619
+  declarations: [EventsMsgComponent, FlightInfoComponent, EventTypeMsgComponent, EventTypeMsgDialogComponent],
   imports: [
     CommonModule,
     MaterialModule
   ],
-<<<<<<< HEAD
-  exports: [EventsMsgComponent, EventTypeMsgComponent, EventTypeMsgDialogComponent]
-=======
-  exports: [EventsMsgComponent, FlightInfoComponent]
->>>>>>> 910dcd7dd37f9903b959e8f312d92d3bad90d619
+
+  exports: [EventsMsgComponent, EventTypeMsgComponent, EventTypeMsgDialogComponent, FlightInfoComponent]
+
+
 })
 export class EventsMessagesModule { }
