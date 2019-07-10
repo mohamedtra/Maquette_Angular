910dcd7dd37f9903b959e8f312d92d3bad90d619
BOUREK <kamel.bourek@cgi.com>
2019-07-10 12:04:25 +0200

    adding flight information component

--- Simulator-App/src/app/components/events-messages/events-messages.module.ts
+++ Simulator-App/src/app/components/events-messages/events-messages.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EventsMsgComponent } from './events-msg/events-msg.component';
 import { MaterialModule } from 'src/app/material.module';
+import { FlightInfoComponent } from './flight-info/flight-info.component';
 
 @NgModule({
-  declarations: [EventsMsgComponent],
+  declarations: [EventsMsgComponent, FlightInfoComponent],
   imports: [
     CommonModule,
     MaterialModule
   ],
-  exports: [EventsMsgComponent]
+  exports: [EventsMsgComponent, FlightInfoComponent]
 })
 export class EventsMessagesModule { }
