c804faf5c83817ae02e724ed7fc9128d09b3ee7e
BOUREK <kamel.bourek@cgi.com>
2019-07-15 10:32:41 +0200

    amelioration affichage

--- Simulator-App/src/app/components/simulator/flight-info/flight-info.component.ts
+++ Simulator-App/src/app/components/simulator/flight-info/flight-info.component.ts
@@ -9,32 +9,36 @@ import {MatTableDataSource} from '@angular/material/table';
 })
 export class FlightInfoComponent implements OnInit {
 
-  displayedColumns: string[] = ['dep_esc', 'dep_term', 'dep_time', 'arr_esc', 'arr_term', 'arr_time'];
+  displayedColumns: string[] = ['leg', 'time_type', 'dep_esc', 'dep_time', 'arr_esc', 'arr_time'];
   // tslint:disable-next-line: no-use-before-declare
   dataSource = new MatTableDataSource<InfoFlight>(ELEMENT_DATA);
-
-  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
 
   constructor() { }
 
   ngOnInit() {
-    this.dataSource.paginator = this.paginator;
   }
 
 }
 
 export interface InfoFlight {
+  leg: string;
   dep_esc: string;
   dep_term: string;
   dep_time: string;
   arr_esc: string;
   arr_term: string;
   arr_time: string;
+  time_type: string;
 }
 
 const ELEMENT_DATA: InfoFlight[] = [
-  {dep_esc: 'NCE', dep_term: 'NCE1', dep_time: '12:00', arr_esc: 'CDG', arr_term: 'CDG1', arr_time: '14:00' },
-  {dep_esc: 'CDG', dep_term: 'CDG1', dep_time: '16:00', arr_esc: 'NCE', arr_term: 'NCE1', arr_time: '20:00' },
-  {dep_esc: 'NCE', dep_term: 'NCE1', dep_time: '22:00', arr_esc: 'CDG', arr_term: 'CDG1', arr_time: '00:00' },
+  {leg: '0', dep_esc: 'NCE', dep_term: 'NCE1', dep_time: '12:00',
+  arr_esc: 'CDG', arr_term: 'CDG1', arr_time: '14:00', time_type: 'schedule' },
+
+  {leg: '1', dep_esc: 'CDG', dep_term: 'CDG1', dep_time: '16:00',
+  arr_esc: 'NCE', arr_term: 'NCE1', arr_time: '20:00', time_type: 'schedule' },
+
+  {leg: '2', dep_esc: 'NCE', dep_term: 'NCE1', dep_time: '22:00',
+  arr_esc: 'CDG', arr_term: 'CDG1', arr_time: '00:00', time_type: 'schedule' },
 
 ];
