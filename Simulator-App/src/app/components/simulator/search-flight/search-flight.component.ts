dfba8f2daf22065b45e587a6a1d7d39dc1f7d559
BOUREK <kamel.bourek@cgi.com>
2019-07-19 14:10:12 +0200

    haha

--- Simulator-App/src/app/components/simulator/search-flight/search-flight.component.ts
+++ Simulator-App/src/app/components/simulator/search-flight/search-flight.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Flight } from 'src/app/models/flight';
+import { FlightLeg } from 'src/app/models/flightleg';
 
 @Component({
   selector: 'app-search-flight-info',
@@ -26,11 +27,36 @@ export class SearchFlightComponent implements OnInit {
     console.log(this.flight.operationalSuffix);
   }
 
-  checkInformation(company: string, flightNumber: string, operationalSuffix: string, flightDate: Date ) {
-    this.company = company;
-    this.flightNumber = flightNumber;
-    this.operationalSuffix = operationalSuffix;
-    this.flightDate = flightDate;
+  checkInformation() {
+    this.flight.company = this.company;
+    this.flight.flightNumber = this.flightNumber;
+    this.flight.operationalSuffix = this.operationalSuffix;
+    this.flight.aircraftRegistration = 'FHBNR';
+    this.flight.aircraftType = '320';
+    this.flight.flightIdentifierDate = this.flightDate;
+
+    this.flight.flightLegs = [];
+
+    const fl1: FlightLeg = new FlightLeg();
+    const fl2: FlightLeg = new FlightLeg();
+
+    fl1.arrPkg = 'B20';
+    fl1.depPkg = 'H02';
+    fl1.depStation = 'NCE1';
+    fl1.arrStation = 'CDG1';
+    fl1.depTime = '07:00 S';
+    fl1.arrTime = '08:45 S';
+
+    fl2.arrPkg = 'H16';
+    fl2.depPkg = 'B20';
+    fl2.depStation = 'CDG1';
+    fl2.arrStation = 'MRS1';
+    fl2.depTime = '10:00 S';
+    fl2.arrTime = '11:45 S';
+
+
+    this.flight.flightLegs.push(fl1);
+    this.flight.flightLegs.push(fl2);
   }
 
 }
